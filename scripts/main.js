ec409356595d93d0c3b2c61398ec956923260093
Will Cook <wellsee83@gmail.com>
2016-03-07 16:05:22 -0600

    Adventurer Mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
 var currentAge = prompt("What is your current age?");
-console.log("What is your current age? " + currentAge);
-
 var retireAge = prompt("What age would you like to retire?");
-console.log("At what age would you like to retire? " + retireAge);
-
 var yearsLeft = (parseFloat(retireAge) - parseFloat(currentAge));
-console.log("You have " + yearsLeft + " years left until you can retire.");
-
 var thisYear = new Date().getFullYear();
 var retireDate = (thisYear + yearsLeft);
-console.log('It\'s ' + thisYear + ', so you can retire in ' + retireDate + '.');
+
+// Returns current and retirement age.
+console.log("What is your current age? " + currentAge);
+console.log("At what age would you like to retire? " + retireAge);
+
+// Returns remaining year/date until retirement.
+if (yearsLeft > 0)	{
+	console.log("You have " + yearsLeft + " years left until you can retire.");
+	console.log('It\'s ' + thisYear + ', so you can retire in ' + retireDate + '.');
+}
+// Returns declaration of retirement.
+if (yearsLeft < 0) {
+	console.log('Congratulations, you can retire already.')
+}
